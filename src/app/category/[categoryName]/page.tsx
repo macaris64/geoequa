928c670251d0acdf8d1e44bff71d108ba9528d8a
mehmet acar <mehmet.acar@udemy.com>
2024-10-05 14:25:20 +0300

    - Refactor category detail page

--- src/app/category/[categoryName]/page.tsx
+++ src/app/category/[categoryName]/page.tsx
@@ -1,53 +1,66 @@
 "use client";
 
 import { useParams, useRouter } from 'next/navigation';
-
 import Table from "@/app/components/table";
 import { allData, categoryDescriptions } from "@/app/data";
 import CategoryHeader from "@/app/components/categoryHeader";
 import DataDropdown from "@/app/components/dataDropdown";
 import Breadcrumb from "@/app/components/breadcrumb";
+import { Key } from 'react';
 
 export default function CategoryPage() {
-    const { categoryName } = useParams();
+    const {categoryName} = useParams();
     const router = useRouter();
     const decodedCategoryName = decodeURIComponent(categoryName.toString());
-    const filteredData = allData.filter(item => item.category === decodedCategoryName);
 
+    const filteredData = getFilteredData(decodedCategoryName);
 
-    const handleCategorySelect = (category: string) => {
-        router.push(`/category/${decodeURIComponent(category)}`);
-    };
-
-    const breadcrumbItems = [
-        { label: 'Home', href: '/' },
-        { label: decodeURIComponent(categoryName.toString()), href: `/category/${categoryName}` }
-    ];
+    const breadcrumbItems = createBreadcrumbItems(categoryName);
 
     return (
         <div>
-            <Breadcrumb items={breadcrumbItems} />
-            <DataDropdown onCategorySelect={handleCategorySelect} selectedCategory={decodedCategoryName} />
+            <Breadcrumb items={breadcrumbItems}/>
+            <DataDropdown onCategorySelect={handleCategorySelect} selectedCategory={decodedCategoryName}/>
             <CategoryHeader
-                title={decodedCategoryName.toString()}
-                description={categoryDescriptions[decodedCategoryName.toString()]}
+                title={decodedCategoryName}
+                description={categoryDescriptions[decodedCategoryName] || 'No description available.'}
             />
-            <div>
-                {filteredData.length > 0 ? (
-                    filteredData.map((item, index) => (
-                        <Table
-                            key={index}
-                            title={item.title}
-                            description={item.description}
-                            columns={Object.keys(item.data[0])}
-                            data={item.data}
-                            isPreview={true}
-                        />
-                    ))
-                ) : (
-                    <div>No tables available for this category.</div>
-                )}
-            </div>
+            <RenderTables filteredData={filteredData}/>
         </div>
     );
+
+    // Helper Functions
+    function handleCategorySelect(category: string) {
+        router.push(`/category/${decodeURIComponent(category)}`);
+    }
+
+    function getFilteredData(category: string) {
+        return allData.filter(item => item.category === category);
+    }
+
+    function createBreadcrumbItems(category: string | string[]) {
+        return [
+            {label: 'Home', href: '/'},
+            {label: decodeURIComponent(category as string), href: `/category/${category}`}
+        ];
+    }
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    function RenderTables({filteredData}) {
+        if (filteredData.length === 0) {
+            return <div>No tables available for this category.</div>;
+        }
+
+        return filteredData.map((item: { title: string; description: string; data: any[]; }, index: Key | null | undefined) => (
+            <Table
+                key={index}
+                title={item.title}
+                description={item.description}
+                columns={Object.keys(item.data[0])}
+                data={item.data}
+                isPreview={true}
+            />
+        ));
+    }
 }
