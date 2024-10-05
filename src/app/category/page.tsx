18f938c7f9da64e11a58937c569153dc740eda1c
mehmet acar <mehmet.acar@udemy.com>
2024-10-05 16:59:32 +0300

    - Fixed navbar

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -2,9 +2,14 @@
 
 import DataDropdown from "@/app/components/dataDropdown";
 import { useRouter } from 'next/navigation';
+import Breadcrumb from "@/app/components/breadcrumb";
 
 export default function Home() {
     const router = useRouter();
+    const breadcrumbItems = [
+        { label: 'Home', href: '/' },
+        { label: 'Category Data', href: '/category' },
+    ];
 
     const handleCategorySelect = (category: string) => {
         router.push(`/category/${decodeURIComponent(category)}`);
@@ -12,6 +17,7 @@ export default function Home() {
 
     return (
         <div>
+            <Breadcrumb items={breadcrumbItems}/>
             <DataDropdown onCategorySelect={handleCategorySelect} selectedCategory=''/>
         </div>
     )
--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,19 +4,27 @@ import Link from 'next/link';
 
 const Navbar = () => {
     return (
-        <nav className="flex justify-between items-center p-5 bg-blue-600 text-white">
+        <nav className="fixed top-0 left-0 right-0 flex justify-between items-center p-5 bg-blue-600 text-white z-10">
             <Link href="/">
                 <h1 className="text-2xl font-bold cursor-pointer">{'GeoEqua'}</h1>
             </Link>
-            <a href="https://www.spaceappschallenge.org/" target="_blank" rel="noopener noreferrer">
-                <Image
-                    src={'/assets/nasa-logo.png'}
-                    alt="NASA Logo"
-                    width={75}
-                    height={100}
-                    className="cursor-pointer"
-                />
-            </a>
+            <div className="flex items-center space-x-8 ml-auto"> {/* ml-auto'yu kaldırdık, ve space-x değerini artırdık */}
+                <Link href="/country">
+                    <span className="cursor-pointer">Countries</span>
+                </Link>
+                <Link href="/category">
+                    <span className="cursor-pointer">Categories</span>
+                </Link>
+                <a href="https://www.spaceappschallenge.org/" target="_blank" rel="noopener noreferrer">
+                    <Image
+                        src={'/assets/nasa-logo.png'}
+                        alt="NASA Logo"
+                        width={75}
+                        height={100}
+                        className="cursor-pointer"
+                    />
+                </a>
+            </div>
         </nav>
     );
 };
